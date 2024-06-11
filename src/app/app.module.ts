c2c58555048439a1ef9ec0e656f813d39ff85320
EnricMS <enricmiralles96@gmail.com>
2024-06-11 20:04:22 +0200

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImagesComponent } from './components/images/images.component';
+import { ImageComponent } from './components/image/image.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImagesComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/components/image/image.component.ts
@@ -0,0 +1,41 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Image } from 'src/app/models/image.interface';
+import { ImagesService } from 'src/app/services/images.service';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.css'],
+})
+export class ImageComponent implements OnInit {
+  image?: Image;
+
+  constructor(
+    private imagesService: ImagesService, 
+    private activatedRoute: ActivatedRoute, 
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const identifier = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log('Identifier -->', identifier);
+
+    if (identifier) {
+      this.imagesService.getImageById(identifier).subscribe((image) => {
+        if (!image) {
+          this.router.navigateByUrl('/');
+          return;
+        }
+        this.image = image;
+        console.log('Image -->', this.image);
+      });
+    } else {
+      this.router.navigateByUrl('/');
+    }
+  }
+}
+
+
+
+
